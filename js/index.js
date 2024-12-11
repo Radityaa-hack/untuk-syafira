63f8eb5f49db758f694165ad97f742c074d2272e
Radityaa-hack <maamski321@gmail.com>
2024-12-12 00:28:08 +0700

    First Commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Happy Birthday Firaaa'.split('')
+const text = 'Happy Birthday Fira'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
